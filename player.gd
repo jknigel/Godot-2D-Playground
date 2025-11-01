45ea74a7f3e9ac80bfe6af443a6aab0d7412564b
Jknigel <ajknigel360@gmail.com>
2025-11-01 15:30:44 +0800

    Development 0.0.2

--- player.gd
+++ player.gd
@@ -1,10 +1,12 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta) -> void:
 	var velocity = Vector2.ZERO
@@ -25,3 +27,22 @@ func _process(delta) -> void:
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
